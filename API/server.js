ac735241d2749124227db5edbc1c3814872b2ef1
Farhan <o3190474834@gmail.com>
2024-08-01 14:38:12 +0500

    first commit

---
+++ API/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from 'express'
+import userRouter from './routes/user.js'
+import recipeRouter from './routes/recipe.js'
+import cors from 'cors'
+const app = express();
+import dotenv from "dotenv"
+dotenv.config()
+
+app.use(bodyParser.json())
+app.use(cors({
+  origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
+
+// userRouter
+app.use('/api', userRouter)
+// recipeRouter
+app.use('/api', recipeRouter)
+
+// mongoose
+//   .connect(
+//     "mongodb://localhost:27017/",
+//   )
+//   .then(() => console.log("MongoDB is Connected..!"))
+//   .catch((err) => console.log(err.message));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to mongoDB")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+const port = 3000;
+app.listen(port, () => console.log(`server is running on port ${port}`));
